dfd6c29645469c830d5fc9972ab48cb30da7c8c6
jazper298 <minatosjutsu@gmail.com>
2021-07-11 22:12:24 +0800

    account partial view

--- lib/modules/account/account_view.dart
+++ lib/modules/account/account_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yestech_flutter/configs/app_configs.dart';
 
 class AccountView extends StatefulWidget {
   const AccountView({key}) : super(key: key);
@@ -10,6 +11,135 @@ class AccountView extends StatefulWidget {
 class _AccountViewState extends State<AccountView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final config = App(context);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            height: config.appHeight(100),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  height: config.appHeight(15),
+                  color: Colors.green,
+                  padding: EdgeInsets.only(
+                    top: 30.0,
+                    left: 15.0,
+                    right: 15.0,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                width: config.appWidth(60),
+                                child: Text(
+                                  'Jasper Tony Atillo',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Container(
+                                width: config.appWidth(60),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      child: Text(
+                                        'Profile',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      width: 10.0,
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward_ios,
+                                      size: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          Image.asset(
+                            'assets/images/user_placeholder.png',
+                            height: 60,
+                            width: 60,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Container(
+                  height: config.appHeight(5),
+                  margin: EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Rate Us'),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Container(
+                  height: config.appHeight(5),
+                  margin: EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Settings'),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Container(
+                  height: config.appHeight(5),
+                  margin: EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Send Feedback'),
+                    ],
+                  ),
+                ),
+                Divider(),
+                Container(
+                  height: config.appHeight(5),
+                  margin: EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                Divider(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/modules/login/login_educator_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LpginEducatorView extends StatefulWidget {
+  const LpginEducatorView({key}) : super(key: key);
+
+  @override
+  _LpginEducatorViewState createState() => _LpginEducatorViewState();
+}
+
+class _LpginEducatorViewState extends State<LpginEducatorView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
